ec31e196cca8d316a0d0b4c9bebf32bdf52be601
sasagawa888 <ken-s@tkcnf.or.jp>
2020-02-07 19:06:58 +0900

    Added forward/2

--- lib/deeppipe2.ex
+++ lib/deeppipe2.ex
@@ -1,18 +1,35 @@
 defmodule Deeppipe2 do
-  @moduledoc """
-  Documentation for `Deeppipe2`.
-  """
+  # common
+  def is_matrix({r,c,dt}) do
+    if is_integer(r) && is_integer(c) && is_list(dt) do
+      true 
+    else 
+      false
+    end 
+  end
 
-  @doc """
-  Hello world.
+  # forward
+  def forward(x, []) do
+    x
+  end
 
-  ## Examples
+  def forward(x, [{:weight, w, _, _} | rest]) do
+    x1 = Cumatrix.mult(x, w)
+    forward(x1, rest)
+  end
 
-      iex> Deeppipe2.hello()
-      :world
+  def forward(x, [{:bias, b, _, _} | rest]) do
+    x1 = Cumatrix.badd(x, b)
+    forward(x1, rest)
+  end
 
-  """
-  def hello do
-    :world
+  def forward(x, [{:function, name} | rest]) do
+    cond do
+      name == :sigmoid -> Cumatrix.activate(x,:sigmoid)
+      name == :tanh -> Cumatrix.activate(x,:tanh)
+      name == :relu -> Cumatrix.activate(x,:relu)
+      name == :softmax -> Cumatrix.activate(x,:softmax)
+      true -> raise "not exist function" 
+    end 
   end
 end
--- test/deeppipe2_test.exs
+++ test/deeppipe2_test.exs
@@ -2,7 +2,5 @@ defmodule Deeppipe2Test do
   use ExUnit.Case
   doctest Deeppipe2
 
-  test "greets the world" do
-    assert Deeppipe2.hello() == :world
-  end
+  
 end
