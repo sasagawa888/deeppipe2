502e1d0f9eb52a09fc8765a2f84f9f6d3cf0c09a
sasagawa888 <ken-s@tkcnf.or.jp>
2020-03-18 16:39:52 +0900

    Added test network for CNN

--- lib/test.ex
+++ lib/test.ex
@@ -26,6 +26,15 @@ defmodule Test do
     |> w(100, 10) |> b(10) |> softmax
   end
 
+  # for CNN test
+  defnetwork init_network4(_x) do
+    _x |> f(5,5) |> full
+    |> w(576,300) |> b(300) |> relu
+    |> w(300,100) |> b(100) |> relu
+    |> w(100,10) |> b(10) |> softmax
+  end
+
+
   def sgd(m, n) do
     IO.puts("preparing data")
     image = MNIST.train_image(3000, :flatten)
@@ -112,4 +121,37 @@ defmodule Test do
     IO.puts(loss)
     sgd1(image, network2, train, m, n - 1)
   end
+
+
+  def cnn(m, n) do
+    IO.puts("preparing data")
+    image = MNIST.train_image(3000, :structure)
+    label = MNIST.train_label_onehot(3000)
+    network = init_network1(0)
+    IO.puts("ready")
+    network1 = sgd1(image, network, label, m, n)
+    test_image = MNIST.test_image(1000, :structure) |> CM.new()
+    test_label = MNIST.test_label(1000)
+    correct = DP.accuracy(test_image, network1, test_label)
+    IO.write("accuracy rate = ")
+    IO.puts(correct)
+    IO.puts("end")
+  end
+
+  def cnn1(_, network, _, _, 0) do
+    network
+  end
+
+  def cnn1(image, network, train, m, n) do
+    {image1, train1} = DP.random_select(image, train, m, 2000)
+    network1 = DP.gradient(image1, network, train1)
+    network2 = DP.learning(network, network1)
+    [y | _] = DP.forward(image1, network2, [])
+    loss = CM.loss(y, train1, :cross)
+    IO.puts(loss)
+    cnn1(image, network2, train, m, n - 1)
+  end
+
+
+
 end
