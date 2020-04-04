aaadf659724520208b98549d32ecb466f0c0b5d2
sasagawa888 <ken-s@tkcnf.or.jp>
2020-04-04 11:27:05 +0900

    Added long network test

--- lib/test.ex
+++ lib/test.ex
@@ -59,6 +59,19 @@ defmodule Test do
     |> w(100, 10) |> b(10) |> softmax
   end
 
+  # long network test
+  # 
+  defnetwork init_network8(_x) do
+    _x
+    |> w(784, 600) |> b(600) |> relu
+    |> w(600, 500) |> b(500) |> relu
+    |> w(500, 400) |> b(400) |> relu
+    |> w(400, 300) |> b(300) |> relu
+    |> w(300, 100) |> b(100) |> relu
+    |> w(100, 10) |> b(10) |> softmax
+  end
+
+
 
 
   def sgd(m, n) do
@@ -237,5 +250,35 @@ defmodule Test do
     IO.puts(loss)
     drop1(image, network2, train, m, n - 1)
   end
+
+  def long(m, n) do
+    IO.puts("preparing data")
+    image = MNIST.train_image(3000, :flatten)
+    label = MNIST.train_label_onehot(3000)
+    network = init_network8(0)
+    IO.puts("ready")
+    network1 = drop1(image, network, label, m, n)
+    test_image = MNIST.test_image(1000, :flatten) |> CM.new()
+    test_label = MNIST.test_label(1000)
+    correct = DP.accuracy(test_image, network1, test_label)
+    IO.write("accuracy rate = ")
+    IO.puts(correct)
+    IO.puts("end")
+  end
+
+  def long1(_, network, _, _, 0) do
+    network
+  end
+
+  def long1(image, network, train, m, n) do
+    {image1, train1} = DP.random_select(image, train, m, 2000)
+    network1 = DP.gradient(image1, network, train1)
+    network2 = DP.learning(network, network1)
+    [y | _] = DP.forward(image1, network2, [])
+    loss = CM.loss(y, train1, :cross)
+    IO.puts(loss)
+    drop1(image, network2, train, m, n - 1)
+  end
+
 
 end
