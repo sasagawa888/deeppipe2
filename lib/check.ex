983bf50d85466b6a0341e599fc2790d571c6ce53
sasagawa888 <ken-s@tkcnf.or.jp>
2020-04-20 18:06:41 +0900

    Added gradient confirmation to Check modlule

--- lib/check.ex
+++ lib/check.ex
@@ -3,7 +3,7 @@ defmodule Check do
   alias Deeppipe, as: DP
   alias Cumatrix, as: CM
 
-  # for grad confarmation
+  # for grad confirmation
   defnetwork test_network0(_x) do
     _x
     |> cf([[[0.1, 0.1], [0.1, 0.1]], [[0.1, 0.1], [0.1, 0.1]]])
@@ -12,77 +12,30 @@ defmodule Check do
     |> softmax
   end
 
-  def fd() do
-    data =
-      [
-        [
-          [
-            [0.1, 0.2, 0.3, 0.4],
-            [0.5, 0.6, 0.7, 0.8],
-            [0.9, 0.10, 0.11, 0.12],
-            [0.13, 0.14, 0.15, 0.16]
-          ],
-          [
-            [0.17, 0.18, 0.19, 0.20],
-            [0.21, 0.22, 0.23, 0.24],
-            [0.25, 0.26, 0.27, 0.28],
-            [0.29, 0.30, 0.31, 0.32]
-          ]
-        ]
-      ]
-      |> CM.new()
-
-    [y|_] = DP.forward(data, test_network0(0),[])
-    y |> CM.to_list()
-  end
-
-  def grad() do
-    data =
-      [
-        [
-          [
-            [0.1, 0.2, 0.3, 0.4],
-            [0.5, 0.6, 0.7, 0.8],
-            [0.9, 0.10, 0.11, 0.12],
-            [0.13, 0.14, 0.15, 0.16]
-          ],
-          [
-            [0.17, 0.18, 0.19, 0.20],
-            [0.21, 0.22, 0.23, 0.24],
-            [0.25, 0.26, 0.27, 0.28],
-            [0.29, 0.30, 0.31, 0.32]
-          ]
-        ]
-      ]
-      |> CM.new()
-
+  def test() do
+    data = CM.rand(1,2,4,4)
     train = [[0.0,1.0,0.0,0.0]] |> CM.new()
     network1 = DP.numerical_gradient(data, test_network0(0), train)
-    network1 |> DP.print()
-  end
+    network2 = DP.gradient(data, test_network0(0), train)
+    test1(network1,network2,1)
+  end 
 
-  def back() do
-    data =
-      [
-        [
-          [
-            [0.1, 0.2, 0.3, 0.4],
-            [0.5, 0.6, 0.7, 0.8],
-            [0.9, 0.10, 0.11, 0.12],
-            [0.13, 0.14, 0.15, 0.16]
-          ],
-          [
-            [0.17, 0.18, 0.19, 0.20],
-            [0.21, 0.22, 0.23, 0.24],
-            [0.25, 0.26, 0.27, 0.28],
-            [0.29, 0.30, 0.31, 0.32]
-          ]
-        ]
-      ]
-      |> CM.new()
+  def test1([],[],_) do true end 
+  def test1([x | xs],[y | ys],n) do
+    cond do
+      (CM.is_matrix(x) && CM.is_matrix(y)) || (CM.is_matrix(x) && CM.is_tensor(y)) -> 
+        if CM.is_near(x,y) do 
+          test1(xs,ys,n+1)
+        else
+          IO.puts(n)
+          x |> CM.to_list() |> IO.inspect()
+          y |> CM.to_list() |> IO.inspect()
+          test1(xs,ys,n+1)
+      end 
+      true -> test1(xs,ys,n+1)
+    end
+  end  
 
-    train = [[0.0,1.0,0.0,0.0]] |> CM.new()
-    network1 = DP.gradient(data, test_network0(0), train)
-    network1 |> DP.print()
-  end
+
+  
 end 
