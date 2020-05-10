8e7746c71280bb046eb0db983e1121fdbca3c2b9
sasagawa888 <ken-s@tkcnf.or.jp>
2020-05-10 11:54:36 +0900

    Added CIFAR comment

--- lib/cifar.ex
+++ lib/cifar.ex
@@ -4,7 +4,7 @@ defmodule CIFAR do
   alias Cumatrix, as: CM
 
   # for CNN test
-  # CIFAR.sgd(100,2000) 20epochs
+  # CIFAR.sgd(400,500) 20epochs mini batch size 400
 
   defnetwork init_network1(_x) do
     _x
@@ -23,7 +23,7 @@ defmodule CIFAR do
     |> softmax
   end
 
-  
+  # sgd/2 train network and save network temp.ex
   def sgd(m, n) do
     image = train_image(10000)
     onehot = train_label_onehot(10000)
@@ -33,7 +33,7 @@ defmodule CIFAR do
     DP.train(network, image, onehot, test_image, test_label, :cross, :sgd, m, n)
   end
 
-  #resgd load network from temp.ex and restart training
+  # resgd/2 load network from temp.ex and restart training
   def resgd(m, n) do
     image = train_image(10000)
     onehot = train_label_onehot(10000)
@@ -44,36 +44,7 @@ defmodule CIFAR do
     DP.retrain("temp.ex", image, onehot, test_image, test_label, :cross, :sgd, m, n)
   end
 
-  def sgd1(m, n) do
-    IO.puts("preparing data")
-    image = train_image(10000) |> CM.new()
-    label = train_label_onehot(10000) |> CM.new()
-    network = init_network1(0)
-    IO.puts("ready")
-    network1 = sgd2(image, network, label, m, n)
-    test_image = test_image(100) |> CM.new()
-    test_label = test_label(100)
-    correct = DP.accuracy(test_image, network1, test_label)
-    IO.write("accuracy rate = ")
-    IO.puts(correct)
-    IO.puts("end")
-  end
-
-  def sgd2(_, network, _, _, 0) do
-    network
-  end
-
-  def sgd2(image, network, train, m, n) do
-    {image1, train1} = CM.random_select(image, train, m)
-    network1 = DP.gradient(image1, network, train1)
-    network2 = DP.learning(network, network1)
-    [y | _] = DP.forward(image1, network2, [])
-    loss = CM.loss(y, train1, :cross)
-    IO.write(n)
-    IO.write(" ")
-    IO.puts(loss)
-    sgd2(image, network2, train, m, n - 1)
-  end
+  
 
   # transfer from train-label to onehot list
   def train_label_onehot(n) do
