3012b384797ef4aecf9a62bc0d7b1906acae09b6
sasagawa888 <ken-s@tkcnf.or.jp>
2020-06-14 09:54:43 +0900

    Modify MNIST Fashion module

--- lib/fashion.ex
+++ lib/fashion.ex
@@ -1,6 +1,7 @@
 defmodule Fashion do
   import Network
   alias Deeppipe, as: DP
+  alias Cumatrix, as: CM
 
   @moduledoc """
   test with Fashion-MNIST dataset.
@@ -69,8 +70,6 @@ defmodule Fashion do
   def readagrad(m, n) do
     image = train_image(60000, :structure)
     onehot = train_label_onehot(60000)
-    # test_image = train_image(10000, :structure)
-    # test_label = train_label(10000)
     test_image = test_image(10000, :structure)
     test_label = test_label(10000)
     DP.retrain("temp.ex", image, onehot, test_image, test_label, :cross, :adagrad, m, n)
@@ -103,77 +102,90 @@ defmodule Fashion do
     DP.retry("temp.ex", image, onehot, test_image, test_label, :cross, :adagrad, m, n)
   end
 
-  # structure from flat vector to matrix(r,c) as 1 channel 
-  def structure(x, r, c) do
-    [structure1(x, r, c)]
-  end
-
-  def structure0(x, r, c) do
-    structure1(x, r, c)
-  end
-
-  def structure1(_, 0, _) do
-    []
-  end
-
-  def structure1(x, r, c) do
-    [Enum.take(x, c) | structure1(Enum.drop(x, c), r - 1, c)]
-  end
-
-  # get n datas from train-label
+  
+  @doc """
+  get n datas from train-label
+  """
   def train_label(n) do
     Enum.take(train_label(), n)
   end
 
-  # transfer from train-label to onehot list
+  @doc """
+  transfer from train-label to onehot list
+  """
   def train_label_onehot(n) do
     Enum.take(train_label(), n) |> Enum.map(fn y -> DP.to_onehot(y, 9) end)
   end
 
-  # get n datas from train-image with normalization
+  @doc """
+  get n datas from train-image with normalization
+  """
   def train_image(n, :structure) do
     train_image()
-    |> Enum.take(n)
-    |> Enum.map(fn x -> structure(DP.normalize(x, 0, 255), 28, 28) end)
+    |> Enum.take(n*28*28)
+    |> DP.normalize(0,255)
+    |> CM.reshape([n,1,28,28])
   end
 
-  # get n datas from train-image as flatten list
+  @doc """
+  get n datas from train-image as flatten list
+  """
   def train_image(n, :flatten) do
     train_image()
-    |> Enum.take(n)
-    |> Enum.map(fn x -> DP.normalize(x, 0, 255) end)
+    |> Enum.take(n*784)
+    |> DP.normalize(0, 255)
+    |> CM.reshape([n,784])
   end
 
-  # get n datas from test-label 
+  @doc """
+  get n datas from test-label 
+  """
   def test_label(n) do
     Enum.take(test_label(), n)
   end
 
-  # transfer from test-label to onehot list
+  @doc """
+  transfer from test-label to onehot list
+  """
   def test_label_onehot(n) do
-    Enum.take(test_label(), n) |> Enum.map(fn y -> DP.to_onehot(y, 9) end)
+    test_label()
+    |> Enum.take(n) 
+    |> Enum.map(fn y -> DP.to_onehot(y, 9) end)
   end
 
-  # get n datas from test-image with normalization as structured list
+  @doc """
+  get n datas from test-image with normalization as structured list
+  """
   def test_image(n) do
     test_image()
-    |> Enum.take(n)
-    |> Enum.map(fn x -> structure(DP.normalize(x, 0, 255), 28, 28) end)
+    |> Enum.take(n*28*28)
+    |> DP.normalize(0, 255)
+    |> CM.reshape([n,1,28,28])
   end
 
+  @doc """
+  get n datas from test-image with normalization as structured list or matrix
+  1st arg is size of data 
+  2nd arg is :structure or :flatten
+  """
   def test_image(n, :structure) do
     test_image()
-    |> Enum.take(n)
-    |> Enum.map(fn x -> structure(DP.normalize(x, 0, 255), 28, 28) end)
+    |> Enum.take(n*28*28)
+    |> DP.normalize(0, 255)
+    |> CM.reshape([n,1,28,28])
   end
 
   # get n datas from train-image as flatten list
   def test_image(n, :flatten) do
     test_image()
-    |> Enum.take(n)
-    |> Enum.map(fn x -> DP.normalize(x, 0, 255) end)
+    |> Enum.take(n*784)
+    |> DP.normalize(0, 255)
+    |> CM.reshape([n,784])
   end
 
+  @doc """
+  get train label data
+  """
   def train_label() do
     {:ok, <<0, 0, 8, 1, 0, 0, 234, 96, label::binary>>} =
       File.read("fashion/train-labels-idx1-ubyte")
@@ -181,13 +193,19 @@ defmodule Fashion do
     label |> String.to_charlist()
   end
 
+  @doc """
+  get train image data
+  """
   def train_image() do
     {:ok, <<0, 0, 8, 3, 0, 0, 234, 96, 0, 0, 0, 28, 0, 0, 0, 28, image::binary>>} =
       File.read("fashion/train-images-idx3-ubyte")
 
-    byte_to_list(image)
+    image |> :binary.bin_to_list()
   end
 
+  @doc """
+  get test label data
+  """
   def test_label() do
     {:ok, <<0, 0, 8, 1, 0, 0, 39, 16, label::binary>>} =
       File.read("fashion/t10k-labels-idx1-ubyte")
@@ -195,26 +213,14 @@ defmodule Fashion do
     label |> String.to_charlist()
   end
 
+  @doc """
+  get test image data
+  """
   def test_image() do
     {:ok, <<0, 0, 8, 3, 0, 0, 39, 16, 0, 0, 0, 28, 0, 0, 0, 28, image::binary>>} =
       File.read("fashion/t10k-images-idx3-ubyte")
 
-    byte_to_list(image)
+    image |> :binary.bin_to_list()
   end
 
-  def byte_to_list(bin) do
-    byte_to_list1(bin, 784, [], [])
-  end
-
-  def byte_to_list1(<<>>, _, ls, res) do
-    [Enum.reverse(ls) | res] |> Enum.reverse()
-  end
-
-  def byte_to_list1(bin, 0, ls, res) do
-    byte_to_list1(bin, 784, [], [Enum.reverse(ls) | res])
-  end
-
-  def byte_to_list1(<<b, bs::binary>>, n, ls, res) do
-    byte_to_list1(bs, n - 1, [b | ls], res)
-  end
 end
--- lib/mnist.ex
+++ lib/mnist.ex
@@ -4,6 +4,7 @@ defmodule MNIST do
   """
   import Network
   alias Deeppipe, as: DP
+  alias Cumatrix, as: CM
 
   # for DNN test sgd
   defnetwork init_network1(_x) do
@@ -56,7 +57,6 @@ defmodule MNIST do
     |> relu
     |> pooling(2, 2)
     |> relu
-    # |> visualizer(1,1)
     |> full
     |> w(1728, 10, {:he,2028}, 0.001)
     |> softmax
@@ -242,77 +242,89 @@ defmodule MNIST do
     DP.train(network, image, onehot, test_image, test_label, :cross, :sgd, m, n)
   end
 
-  # structure from flat vector to matrix(r,c) as 1 channel 
-  def structure(x, r, c) do
-    [structure1(x, r, c)]
-  end
-
-  def structure0(x, r, c) do
-    structure1(x, r, c)
-  end
-
-  def structure1(_, 0, _) do
-    []
-  end
-
-  def structure1(x, r, c) do
-    [Enum.take(x, c) | structure1(Enum.drop(x, c), r - 1, c)]
-  end
-
-  # get n datas from train-label
+  @doc """
+  get n datas from train-label
+  """
   def train_label(n) do
     Enum.take(train_label(), n)
   end
 
-  # transfer from train-label to onehot list
+  @doc """
+  transfer from train-label to onehot list
+  """
   def train_label_onehot(n) do
     Enum.take(train_label(), n) |> Enum.map(fn y -> DP.to_onehot(y, 9) end)
   end
 
-  # get n datas from train-image with normalization
+  @doc """
+  get n datas from train-image with normalization
+  """
   def train_image(n, :structure) do
     train_image()
-    |> Enum.take(n)
-    |> Enum.map(fn x -> structure(DP.normalize(x, 0, 255), 28, 28) end)
+    |> Enum.take(n*28*28)
+    |> DP.normalize(0,255)
+    |> CM.reshape([n,1,28,28])
   end
 
-  # get n datas from train-image as flatten list
+  @doc """
+  get n datas from train-image as flatten list
+  """
   def train_image(n, :flatten) do
     train_image()
-    |> Enum.take(n)
-    |> Enum.map(fn x -> DP.normalize(x, 0, 255) end)
+    |> Enum.take(n*784)
+    |> DP.normalize(0, 255)
+    |> CM.reshape([n,784])
   end
 
-  # get n datas from test-label 
+  @doc """
+  get n datas from test-label 
+  """
   def test_label(n) do
     Enum.take(test_label(), n)
   end
 
-  # transfer from test-label to onehot list
+  @doc """
+  transfer from test-label to onehot list
+  """
   def test_label_onehot(n) do
-    Enum.take(test_label(), n) |> Enum.map(fn y -> DP.to_onehot(y, 9) end)
+    test_label()
+    |> Enum.take(n) 
+    |> Enum.map(fn y -> DP.to_onehot(y, 9) end)
   end
 
-  # get n datas from test-image with normalization as structured list
+  @doc """
+  get n datas from test-image with normalization as structured list
+  """
   def test_image(n) do
     test_image()
-    |> Enum.take(n)
-    |> Enum.map(fn x -> structure(DP.normalize(x, 0, 255), 28, 28) end)
+    |> Enum.take(n*28*28)
+    |> DP.normalize(0, 255)
+    |> CM.reshape([n,1,28,28])
   end
 
+  @doc """
+  get n datas from test-image with normalization as structured list or matrix
+  1st arg is size of data 
+  2nd arg is :structure or :flatten
+  """
   def test_image(n, :structure) do
     test_image()
-    |> Enum.take(n)
-    |> Enum.map(fn x -> structure(DP.normalize(x, 0, 255), 28, 28) end)
+    |> Enum.take(n*28*28)
+    |> DP.normalize(0, 255)
+    |> CM.reshape([n,1,28,28])
   end
 
   # get n datas from train-image as flatten list
   def test_image(n, :flatten) do
     test_image()
-    |> Enum.take(n)
-    |> Enum.map(fn x -> DP.normalize(x, 0, 255) end)
+    |> Enum.take(n*784)
+    |> DP.normalize(0, 255)
+    |> CM.reshape([n,784])
   end
 
+  @doc """
+  get train label data
+  """
   def train_label() do
     {:ok, <<0, 0, 8, 1, 0, 0, 234, 96, label::binary>>} =
       File.read("mnist/train-labels-idx1-ubyte")
@@ -320,38 +332,35 @@ defmodule MNIST do
     label |> String.to_charlist()
   end
 
+  @doc """
+  get train image data
+  """
   def train_image() do
     {:ok, <<0, 0, 8, 3, 0, 0, 234, 96, 0, 0, 0, 28, 0, 0, 0, 28, image::binary>>} =
       File.read("mnist/train-images-idx3-ubyte")
 
-    byte_to_list(image)
+    image |> :binary.bin_to_list()
   end
 
+  @doc """
+  get test label data
+  """
   def test_label() do
-    {:ok, <<0, 0, 8, 1, 0, 0, 39, 16, label::binary>>} = File.read("mnist/t10k-labels-idx1-ubyte")
+    {:ok, <<0, 0, 8, 1, 0, 0, 39, 16, label::binary>>} =
+      File.read("mnist/t10k-labels-idx1-ubyte")
+
     label |> String.to_charlist()
   end
 
+  @doc """
+  get test image data
+  """
   def test_image() do
     {:ok, <<0, 0, 8, 3, 0, 0, 39, 16, 0, 0, 0, 28, 0, 0, 0, 28, image::binary>>} =
       File.read("mnist/t10k-images-idx3-ubyte")
 
-    byte_to_list(image)
+    image |> :binary.bin_to_list()
   end
 
-  def byte_to_list(bin) do
-    byte_to_list1(bin, 784, [], [])
-  end
 
-  def byte_to_list1(<<>>, _, ls, res) do
-    [Enum.reverse(ls) | res] |> Enum.reverse()
-  end
-
-  def byte_to_list1(bin, 0, ls, res) do
-    byte_to_list1(bin, 784, [], [Enum.reverse(ls) | res])
-  end
-
-  def byte_to_list1(<<b, bs::binary>>, n, ls, res) do
-    byte_to_list1(bs, n - 1, [b | ls], res)
-  end
 end
